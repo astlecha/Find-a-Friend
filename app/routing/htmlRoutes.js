2312288546000fc83fae2cf596771f5b8ad15d7a
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-22 15:53:22 -0700

    sets up html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+//Package for retrieving html file path
+var path = require('path');
+
+module.exports = function(app) {
+
+	//Get path to the survey html
+	app.get('/survey', function(req,res){
+		res.sendFile(path.join(__dirname, '..public/survey.html'));
+	});
+
+	//Use path to homepage html
+	app.use('/', function(req,res){
+		res.sendFile(path.join(__dirname, '..public/home.html'));
+	});
+
+}
--- server.js
+++ server.js
@@ -3,3 +3,14 @@ npm express = require('express');
 npm bodyParser = require('body-parser');
 npm path = require('path');
 
+//Configure server
+var app = express();
+
+//Define port
+var PORT = 8080;
+
+//Interpret express data with body-parser package
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
