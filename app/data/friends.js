5b5eeb1f4d02438a8bfbeeb52f74944402c67425
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-24 21:45:44 -0700

    score addition logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,11 @@
 var friends = [
 	{	
-		name: 'Josh',
-		photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+		name: 'Peyton',
+		photo: 'https://photos.smugmug.com/Old-Mediacenter-Slideshows/006-BRONCOS/20160108-Peyton-Manning-throug/i-NsKKrbg/0/47753bcf/XL/010815_peyton_manning_03-XL.jpg',
 		scores:[5,1,4,4,5,1,2,5,4,1]
 	}, {
-		name: 'Alice',
-		photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+		name: 'Kylie',
+		photo: 'http://img.usmagazine.com/660-width/kylie-jenner-2-7f6299b1-1fd2-4ef0-aa94-c75722d1d0de.jpg',
 		scores:[3,3,4,4,1,3,5,4,5,2]
 	}
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,5 +11,38 @@ module.exports = function(app){
 	//Set up API post request for new user's data
 	app.post('/api/friends', function(req,res){
 		friendsData.push(req.body);
+
+		var newUserScores = req.body.scores
+		var newAddedScores;
+		var addedScores;
+		
+		for (var i = newUserScores.length - 1; i >= 0; i--) {
+				newAddedScores = (newUserScores[i])+=(newUserScores[i]+1);
+		};
+
+
+		//Loop through user's scores and add
+		for (var i = userScores.length - 1; i >= 0; i--) {
+			addedScores = (userScores[i])+=(userScores[i+1]);
+		};
+
+		var subtractionTotal = addedScores-newAddedScores;
+
+		//Make negative number positive
+		if(subtractionTotal<0){
+			subtractionTotal * (-1);
+		}
+
+
+		//Loop through all users
+		for (var i = 0; i < friendsData.length; i++) {
+			
+			var prevUser = friendsData[i-1]
+			var userScores = friendsData[i].scores;
+		}
+		
+
+
+
 	});
 };
