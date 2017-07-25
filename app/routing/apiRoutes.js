577bb301f127b818d1bb7c98e7a3d4a81456771a
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-24 21:55:47 -0700

    requires path for apipaths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,6 @@
 //Load table data array from friends.js
 var friendsData = require('../data/friends.js');
+var path = require('path');
 
 //Route
 module.exports = function(app){
@@ -10,38 +11,34 @@ module.exports = function(app){
 
 	//Set up API post request for new user's data
 	app.post('/api/friends', function(req,res){
-		friendsData.push(req.body);
-
-		var newUserScores = req.body.scores
-		var newAddedScores;
-		var addedScores;
 		
-		for (var i = newUserScores.length - 1; i >= 0; i--) {
-				newAddedScores = (newUserScores[i])+=(newUserScores[i]+1);
-		};
+		//Grab entered information
+		var info = req.body;
+		var userName = info.name;
+		var userPhoto = info.photo;
+		var userScores = info.scores;
 
+		var totalDifference = 0;
 
-		//Loop through user's scores and add
-		for (var i = userScores.length - 1; i >= 0; i--) {
-			addedScores = (userScores[i])+=(userScores[i+1]);
-		};
-
-		var subtractionTotal = addedScores-newAddedScores;
-
-		//Make negative number positive
-		if(subtractionTotal<0){
-			subtractionTotal * (-1);
+		//Set up object for best match
+		var bestMatch = {
+			name: '',
+			photo: '',
+			matchDifference: 0;
 		}
-
-
-		//Loop through all users
+		
+		//Loop through each friend object to get each scores array
 		for (var i = 0; i < friendsData.length; i++) {
-			
-			var prevUser = friendsData[i-1]
-			var userScores = friendsData[i].scores;
-		}
-		
+			totalDifference=0;
 
+			//Loop through 10 diff scores
+			for (var t = 0; t < 10; t++) {
+				totalDifference +=
+			}
+
+		}
+			
+		friendsData.push(info);
 
 
 	});
